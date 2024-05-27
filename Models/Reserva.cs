b8c9f8de4572519a4ab139a2a75a6891a2e164fe
Lucas Seixas <lucasseixaslima@yahoo.com.br>
2024-05-27 15:30:22 -0300

    Cadastro Hospedes implementado

--- Models/Reserva.cs
+++ Models/Reserva.cs
@@ -15,16 +15,14 @@ namespace DesafioProjetoHospedagem.Models
 
         public void CadastrarHospedes(List<Pessoa> hospedes)
         {
-            // TODO: Verificar se a capacidade é maior ou igual ao número de hóspedes sendo recebido
-            // *IMPLEMENTE AQUI*
-            if (true)
+            bool cabe = hospedes.Count <= Suite.Capacidade;
+            if (cabe)
             {
                 Hospedes = hospedes;
             }
             else
             {
-                // TODO: Retornar uma exception caso a capacidade seja menor que o número de hóspedes recebido
-                // *IMPLEMENTE AQUI*
+                throw new Exception("A quantidade de hospedes não pode ser maior que a capacidade da suite");
             }
         }
 
